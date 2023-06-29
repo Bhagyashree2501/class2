4cdde9d8899dc6ae6e4cc99327116d5b5679a6a1
Bhagyashree2501 <bhagyashreewankhede25@gmail.com>
2023-06-29 11:49:21 +0530

    completed states

---
+++ src/component/Ex3.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+//state => internal variable of react, number, string , boolean , object, , array
+
+const Ex3 = function (props) {
+    //useState => react hook
+    //const [state, handler] = useState(initial value)
+
+    const [x,setX] = useState(123)
+    const [title,setxTitle] = useState("Welcome to react")
+    const [view,setView] = useState(true)
+    
+    const [user,setUser] = useState({
+        name: 'Raju',
+        email: 'raju@gmail.com',
+        age: 24
+    })
+
+    const [colors,setColors] = useState(['red', 'blue', 'green'])
+
+    return (
+        <div>
+            <h3> state in Functional Component</h3>
+            <h4> var x = { x } </h4>
+            <hr/>
+            <h4> { view ? "Say true" : "Say False"}</h4>
+            <hr/>
+
+            <h4> { user.name }, { user.email } and { user.age } years </h4>
+
+            <hr/>
+
+            <ol>
+                {
+                    colors.map((item,index) => {
+                        return (
+                            <li key={index}> { item } </li>
+                        )
+                        })
+                    }
+            </ol>
+        </div>
+    )
+
+}
+
+export default Ex3
---
+++ src/component/Ex4.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+class Ex4 extends React.Component {
+    constructor(props) {
+        super(props);
+        //state
+
+this.state = {
+    x: 123,
+    y: 24,
+    title: 'Welcome to string Example',
+    view : false,
+    colors: ['red', 'green', 'blue'],
+    user: {
+        name: 'raju',
+        email: 'raju@gamil.com',
+        age: 32
+    }
+  }     
+}
+
+render() {
+    return (
+        <div>
+            <h3> states in Class Component </h3>
+            <h3> x = {this.state.x} </h3>
+            <h3> y = {this.state.y} </h3>
+            <h3> {this.state.title} </h3>
+            <h3> {this.state.view ? "Say True" : "Say False" } </h3>
+            <hr/>
+
+            <ol>
+                {
+                    this.state.colors.map((item,index)  => {
+                        return (
+                            <li key={index}> {item} </li>
+                        )
+                    })
+                }
+            </ol>
+
+            <hr/>
+
+            <h3> user name = { this.state.user.name} </h3>
+            <h3> user email = { this.state.user.email} </h3>
+            <h3> user age = { this.state.user.age} years</h3>
+            
+        </div>
+    )
+ }
+}
+
+export default Ex4
